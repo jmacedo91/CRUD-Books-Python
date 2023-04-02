4ddff5ff90cd367a100bae950370f4e2c87a095b
jmacedo91 <jonathan.silvamacedo@gmail.com>
2023-04-02 11:55:48 -0300

    Finishing the GUI

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# CRUD-Books-Python" 
+# 📚 Biblioteca CRUD in Python 📚
+
+Este projeto foi desenvolvido seguindo a orientação do canal
+["Usando Python"](https://www.youtube.com/@usandopython)
+
+- [CRUD em Python com SQLite | Formulário Python Tkinter | parte #01](https://www.youtube.com/watch?v=7N25wyyJ7pc)
+- [CRUD em Python com SQLite | Formulário Python Tkinter | parte #02](https://www.youtube.com/watch?v=bqHbY9cJZbo)
+- [CRUD em Python com SQLite | Formulário Python Tkinter | parte #03](https://www.youtube.com/watch?v=2uYqJVe_0RU)
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # Importando Tkinter
 from tkinter import *
-from tkinter import font
-from tkcalendar import Calendar, DateEntry
+from tkcalendar import DateEntry
 
 ################# cores ###############
-co0 = "#f0f3f5"  # Preta
+co0 = "#f0f3f5"  # cinza
 co1 = "#feffff"  # branca
 co2 = "#4fa882"  # verde
 co3 = "#38576b"  # valor
@@ -14,29 +13,30 @@ co6 = "#038cfc"   # azul
 co7 = "#ef5350"   # vermelha
 co8 = "#263238"   # + verde
 co9 = "#e9edf5"   # sky blue
+co10 = "#000000"  # preta
 
 ################# Criando Janela ###############
 
 janela = Tk()
-janela.title("")
-janela.geometry('1043x513')
+janela.title("Johnny's Library")
+janela.geometry('1043x543')
 janela.configure(background=co9)
 janela.resizable(width=FALSE, height=FALSE) # Bloqueia a alteração do geometry
 
 ################# Dividindo a Janela ###############
 
-frame_cima = Frame(janela, width=310, height=50, bg=co2, relief='flat')
+frame_cima = Frame(janela, width=310, height=50, bg=co10, relief='flat')
 frame_cima.grid(row=0, column=0)
 
-frame_baixo = Frame(janela, width=310, height=463, bg=co1, relief='flat')
+frame_baixo = Frame(janela, width=310, height=493, bg=co1, relief='flat')
 frame_baixo.grid(row=1, column=0, sticky=NSEW, padx=0, pady=1)
 
-frame_direita = Frame(janela, width=588, height=463, bg=co1, relief='flat')
+frame_direita = Frame(janela, width=588, height=493, bg=co1, relief='flat')
 frame_direita.grid(row=0, column=1, rowspan=2, padx=1, sticky=NSEW)
 
 ################# Label Cima ###############
 
-app_nome = Label(frame_cima, text="Johnny's Library", anchor=NW, font='Ivy 13 bold', bg=co2, fg=co1, relief='flat')
+app_nome = Label(frame_cima, text="Johnny's Library", anchor=NW, font='Ivy 13 bold', bg=co10, fg=co1, relief='flat')
 app_nome.place(x=10, y=20)
 
 ################# Configurando Frame Baixo ###############
@@ -83,8 +83,26 @@ label_data.place(x=10, y=310)
 entry_data = DateEntry(frame_baixo, width=42, background='darkblue', foreground='white', borderwidth=2)
 entry_data.place(x=15, y=340)
 
+# Consulta
+
+label_consulta = Label(frame_baixo, text='Consulta *', anchor=NW, font='Ivy 10 bold', bg=co1, fg=co4, relief='flat')
+label_consulta.place(x=10, y=370)
+
+entry_consulta = Entry(frame_baixo, width=45, justify='left', relief='solid')
+entry_consulta.place(x=15, y=400)
+
+################# Botões do Frame Baixo ###############
+
+insert_button = Button(frame_baixo, text="Inserir", width=10, font='Ivy 9 bold', bg=co6, fg=co1, relief='raised',
+                       overrelief='ridge')
+insert_button.place(x=15, y=440)
+
+update_button = Button(frame_baixo, text="Atualizar", width=10, font='Ivy 9 bold', bg=co2, fg=co1, relief='raised',
+                       overrelief='ridge')
+update_button.place(x=112, y=440)
+
+delete_button = Button(frame_baixo, text="Deletar", width=10, font='Ivy 9 bold', bg=co7, fg=co1, relief='raised',
+                       overrelief='ridge')
+delete_button.place(x=209, y=440)
+
 janela.mainloop()
-
-
-
-
